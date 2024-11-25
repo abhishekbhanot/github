c8bdf5252db5c8957aaaf02a06f4fe0dc9b5ce33
abhishekbhanot <abhishekbhanot756@gmail.com>
2024-11-25 18:52:28 +0530

    added new arrow function in main.js

--- script/main.js
+++ script/main.js
@@ -1,4 +1,9 @@
-let headiing=document.querySelector("h1");
+let but=document.querySelector("button");
 
+
+let headiing=document.querySelector("h1");
+but.addEventListener("click",()=>{
     let myName=prompt("enter your name")
     headiing.textContent=`hello welcome to our github page  , ${myName}`
+
+    })
